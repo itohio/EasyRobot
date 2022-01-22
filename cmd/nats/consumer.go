514e6d94587445d8cc19f7ccfff95e34949536fd
Andrius Mikonis <andrius.mikonis@gmail.com>
2022-01-22 11:33:20 +0200

    ADD: Examples.

--- cmd/nats/consumer.go
+++ cmd/nats/consumer.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"context"
+
+	"github.com/itohio/EasyRobot/pkg/core/pipeline"
+	"github.com/itohio/EasyRobot/pkg/core/pipeline/steps"
+	"github.com/itohio/EasyRobot/pkg/core/pipeline/steps/fps"
+	"github.com/itohio/EasyRobot/pkg/core/plugin"
+	"github.com/itohio/EasyRobot/pkg/core/store"
+	"github.com/itohio/EasyRobot/pkg/vision/display"
+	"github.com/itohio/EasyRobot/pkg/vision/writer"
+)
+
+func setupConsumer(ctx context.Context, pipe pipeline.Pipeline, nc pipeline.Step, out, ext string, block, hide bool) (<-chan store.Store, error) {
+	fps, err := fps.New(fps.WithNumFrames(10), plugin.WithBlocking(block))
+	if err != nil {
+		panic(err)
+	}
+
+	fout, err := steps.NewFanOut(plugin.WithBlocking(block), plugin.WithClose(false))
+	if err != nil {
+		panic(err)
+	}
+
+	output, err := display.NewGoCV(
+		plugin.WithClose(out != ""),
+		display.WithKey(store.IMAGE),
+		plugin.WithBlocking(block),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	keys, err := pipe.ConnectSteps(nc, fps, fout, output)
+	if err != nil {
+		panic(err)
+	}
+
+	var sink pipeline.Step
+	if out == "" {
+		sink, err = writer.NewNull(plugin.WithClose(hide))
+	} else {
+		sink, err = writer.NewGoCV(plugin.WithClose(hide), writer.WithKey(store.IMAGE), writer.WithExtension(ext), writer.WithPrefix(out))
+	}
+	_, err = pipe.ConnectSteps(fout, sink)
+	if err != nil {
+		panic(err)
+	}
+
+	return keys, nil
+}
--- cmd/nats/main.go
+++ cmd/nats/main.go
@@ -6,16 +6,9 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/itohio/EasyRobot/pkg/core/options"
 	"github.com/itohio/EasyRobot/pkg/core/pipeline"
-	"github.com/itohio/EasyRobot/pkg/core/pipeline/steps"
-	"github.com/itohio/EasyRobot/pkg/core/pipeline/steps/fps"
-	"github.com/itohio/EasyRobot/pkg/core/plugin"
 	"github.com/itohio/EasyRobot/pkg/core/store"
 	"github.com/itohio/EasyRobot/pkg/core/transport/nats"
-	"github.com/itohio/EasyRobot/pkg/vision/display"
-	"github.com/itohio/EasyRobot/pkg/vision/reader"
-	"github.com/itohio/EasyRobot/pkg/vision/writer"
 	natsgo "github.com/nats-io/nats.go"
 )
 
@@ -57,68 +50,11 @@ func main() {
 		panic(err)
 	}
 
+	var keys <-chan store.Store
 	if *consume {
-		setupConsumer(ctx, pipe, ncStep, *out, *ext, *hide)
+		keys, err = setupConsumer(ctx, pipe, ncStep, *out, *ext, *block, *hide)
 	} else {
-		setupProducer(ctx, pipe, ncStep, *width, *height, *device, *file, *imagesPath)
-	}
-
-	var input pipeline.Step
-	var output pipeline.Step
-	var sink pipeline.Step
-	var err error
-
-	opts := []options.Option{}
-
-	if len(*imagesPath) != 0 {
-		paths := ReadFileList(*imagesPath)
-		opts = append(opts, reader.WithReaderGoCV(paths))
-	} else if len(*file) != 0 {
-		opts = append(opts, reader.WithVideoReaderGoCV(*file))
-	} else {
-		opts = append(opts, reader.WithDeviceReaderGoCVResolution(*device, *width, *height))
-	}
-
-	opts = append(opts, plugin.WithBlocking(*block))
-	input, err = steps.NewReader(opts...)
-	if err != nil {
-		panic(err)
-	}
-
-	fps, err := fps.New(fps.WithNumFrames(10), plugin.WithBlocking(*block))
-	if err != nil {
-		panic(err)
-	}
-
-	fout, err := steps.NewFanOut(plugin.WithBlocking(*block), plugin.WithClose(false))
-	if err != nil {
-		panic(err)
-	}
-
-	if !*hide {
-		output, err = display.NewGoCV(
-			plugin.WithClose(*out != ""),
-			display.WithKey(store.IMAGE),
-			plugin.WithBlocking(*block),
-		)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	keys, err := pipe.ConnectSteps(input, fps, fout, output)
-	if err != nil {
-		panic(err)
-	}
-
-	if *out == "" {
-		sink, err = writer.NewNull(plugin.WithClose(*hide))
-	} else {
-		sink, err = writer.NewGoCV(plugin.WithClose(*hide), writer.WithKey(store.IMAGE), writer.WithExtension(*ext), writer.WithPrefix(*out))
-	}
-	_, err = pipe.ConnectSteps(fout, sink)
-	if err != nil {
-		panic(err)
+		keys, err = setupProducer(ctx, pipe, ncStep, *width, *height, *device, *file, *imagesPath, *out, *ext, *block, *hide)
 	}
 
 	pipe.Run(ctx)
